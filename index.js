6d7ae86df4dac6b6a79b4f66c70b869deaa40582
Brandon <bholmdev@gmail.com>
2023-02-23 09:54:05 -0600

    formatting changes

--- index.js
+++ index.js
@@ -29,31 +29,22 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
                 })  
 
                 dboper.updateDocument(
-                    db,
-                    { name: "Breadcrumb Trail Campground" },
-                    { description: "Updated Test Description" },"campsites", result => {
+                    db, { name: "Breadcrumb Trail Campground" }, { description: "Updated Test Description" },"campsites",
+                    result => {
                         console.log("Updated Document Count: ", result.result.nModified);
 
                         dboper.findDocuments(
-                            db,
-                            "campsites",
+                            db, "campsites",
                             docs => {
                                 console.log("Found Documents: ", docs);
 
-                                dboper.removeDocument(
-                                    db,
-                                    { name: "Breadcrumb Trail Campground" },
-                                    "campsites",
+                                dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, "campsites",
                                     result => {
                                         console.log("Deleted Document Count: ", result.deletedCount);
                                         client.close();
-                                    }
-                                );
-                            }
-                        );
-                    }
-                )
-            }
-        );
+                                });
+                        });
+                });
+        });
     });
 });
